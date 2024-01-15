cdca56e98bd0c9267bbe442aa087e5a1207bbe37
n8shaw <nathanjshaw88@gmail.com>
2024-01-15 11:18:14 -0700

    Add project files.

---
+++ DiceRoller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission_2
+{
+    internal class DiceRoller
+    {
+        
+
+        //constructor
+        public DiceRoller()
+        {
+           
+        }
+
+        public int[] RollDie(int numRolls)
+        {
+            int[] results = new int[11];
+            Random random = new Random();
+
+            for (int i = 0; i < numRolls; i++)
+            {
+                
+                int randomRoll = random.Next(2,13);
+
+                results[randomRoll-2]++;
+            }
+
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Mission_2;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DiceRoller dr = new DiceRoller();
+        int rollCount = 0;
+        int[] results = new int[11];
+
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolles would you like to simulate?");
+
+
+        rollCount = int.Parse(System.Console.ReadLine());
+
+        results = dr.RollDie(rollCount);
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            // Calculate the percentage of rolls for the current result
+            double percentage = (results[i] / (double)rollCount) * 100;
+
+            // Print the result and corresponding stars
+            System.Console.Write((i + 2) + ": ");
+
+            // Print a '*' for every 1%
+            int numStars = (int)Math.Floor(percentage); // Use Ceiling to round up to the nearest integer
+            for (int j = 0; j < numStars; j++)
+            {
+                System.Console.Write("*");
+            }
+
+            System.Console.WriteLine(); // Move to the next line after printing stars
+        }
+
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+    }
+}
